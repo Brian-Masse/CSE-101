4878e8edcbec6a7be4face91e59b911eb6b40a8b
Brian Masse <brianmasse@Brians-MacBook-Pro.local>
2025-11-25 20:26:55 -0500

    added hw6 question 1

--- hw/hw6/hw6.py
+++ hw/hw6/hw6.py
@@ -1,7 +1,7 @@
 
 # pass in the list of pairs, the size of the list, and half the total weight
 # The final piece can be precomputed in linear time. 
-weighted_median(list L, size n, WM):
+def weighted_median(list L, size n, WM):
 
     # Find the median of the given list (by value, not weight)
     # Using the select algorithm from class
@@ -33,3 +33,41 @@ weighted_median(list L, size n, WM):
         return weighted_median(WL, WL.size(), WM)
     else:
         return weighted_median(WR, WR.size(), WM)
+
+
+
+# pass in a node to start on. In the specialized problem, this will always be the root. 
+# The function returns 2 things:
+#   1. The highest weight independent set with the root
+#   2. The highest weight independent set without the root
+# The answer is the set with the larger weight at the end. 
+def WeightedSets(node):
+
+    if node has no children:
+        return ( [node], [] )
+
+    if node has 1 child:
+        return ( [node], [child] )
+    
+    left_result = WeightedSets(leftChild)
+    right_result = WeightedSets(rightChild)
+
+    with_left = left_result[0]; without_left = left_result[1]
+    with_right = right_result[0]; without_right = right_result[1]
+
+    with_node = [], without_node = []
+
+    # Generate the optimal set containing the current node
+    with_node += without_left + without_right + node
+
+    # generate the optimal set without the current node
+    if weight(with_right) > weight(without_right):
+        without_node += with_right
+    else:
+        without_node += without_right
+    if weight(with_left) > weight(without_left):
+        without_node += with_left
+    else:
+        without_node += without_left
+    
+    return with_node, without_node
