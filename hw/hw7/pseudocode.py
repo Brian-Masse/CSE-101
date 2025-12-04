a111b4b9f16a0fa366546ebe9c7fdb5a1a2ce0f3
Brian Masse <brianmasse@Mac-8723.lan>
2025-12-04 00:18:44 -0800

    added question 5

--- hw/hw7/pseudocode.py
+++ hw/hw7/pseudocode.py
@@ -19,6 +19,8 @@ def MWCS(root, k) -> smallest:
     # if a subtree does not contain k nodes (ie. does not satisfy constraints of potential solution)
     # return infinity
     smallest = infinity
+    weight = [infinity]
+    size = [infinity]
 
     for v in (post-order traverse root):
         if |v.children| == 0:
@@ -38,3 +40,27 @@ def MWCS(root, k) -> smallest:
             smallest = min(smallest, weight[v])
     
     return smallest
+
+# MARK: KV()
+def KV(I1,...In, U):
+
+    sort (I1,...In) by the value ratio of each item
+
+    # create memoized arrays
+    U' = [infinity]; U'[0] = U
+    V = [0]
+    P = [0]
+
+    # loop through each item
+    for Ii in I1,...In:
+        
+        # if the previous item fully depleted the budget, all remaining item counts are 0
+        # and the function should return
+        if U'[i - 1] == 0: 
+            break
+
+        P[i] = floor( U'[i-1] / ci )
+        V[i] = V[i - 1] + P[i] * vi
+        U'[i] = U'[i - 1] - P[i] * ci
+
+    return P
